0e727bec2c0d47fb4e899d31e4c1554ef6ff3456
Keoma Kevin Baudin <keoma4@gmail.com>
2024-01-18 22:44:11 -0300

    project files

---
+++ src/com/pluralsight/calcengine/MathOperation.java
@@ -0,0 +1,18 @@
+package com.pluralsight.calcengine;
+
+public enum MathOperation {
+    ADD('+'),
+    SUBTRACT('-'),
+    MULTIPLY('*'),
+    DIVIDE('/');
+
+    private final char symbol;
+
+    private MathOperation(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
